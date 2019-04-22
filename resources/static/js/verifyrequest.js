49a8afdcdd81cca8cc836f851b558f6a7f31281a
vasipara4 <billprig@gmail.com>
2019-04-22 17:26:22 +0300

    Bugs

--- resources/static/js/verifyrequest.js
+++ resources/static/js/verifyrequest.js
@@ -4,17 +4,16 @@ $( document ).ready(function() {
     $("#verifyForm").submit(function(event) {
       event.preventDefault();
       //var hashInEthereum;
-      var verifyHtml;
+      var verifyHtml = "False";
       var testingData = $("#verify_measurement").val();
       var testingId = $("#verify_id").val();
       contract.methods.verifyHash(testingData,testingId).call({from : account}).then(function(result){
+        console.log(result);
         if (result) {
-          contract.methods.getDataDetails(testingId).call({from: account}).then(function(results){
-            verifyHtml = "True" +"<br>Account: " + results[0] + "<br>Hash: " + results[1] + "<br>Unix Date: " + results[2];
+          contract.methods.getDataDetails(testingId).call({from: account}).then(function(result){
+            console.log(response);
+            verifyHtml = "True" +"<br>Account: " + result[0] + "<br>Hash: " + result[1] + "<br>Unix Date: " + result[2];
           });
-        }
-        else {
-          verifyHtml = "False";
         }
          $("#getVerifyDiv").html("Verification: " + verifyHtml);
       });
