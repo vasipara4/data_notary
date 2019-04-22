b75163a2160d5a5a599b74dc4dceba6e353e610a
vasipara4 <billprig@gmail.com>
2019-04-22 17:35:07 +0300

    Promises JS Fixed

--- resources/static/js/verifyrequest.js
+++ resources/static/js/verifyrequest.js
@@ -1,23 +1,37 @@
-$( document ).ready(function() {
-
-    // GET REQUEST
-    $("#verifyForm").submit(function(event) {
-      event.preventDefault();
-      //var hashInEthereum;
-      var verifyHtml = "False";
-      var testingData = $("#verify_measurement").val();
-      var testingId = $("#verify_id").val();
-      contract.methods.verifyHash(testingData,testingId).call({from : account}).then(function(result){
+$(document).ready(function() {
+  // GET REQUEST
+  $("#verifyForm").submit(function(event) {
+    event.preventDefault();
+    //var hashInEthereum;
+    var verifyHtml = "False";
+    var testingData = $("#verify_measurement").val();
+    var testingId = $("#verify_id").val();
+    contract.methods
+      .verifyHash(testingData, testingId)
+      .call({ from: account })
+      .then(function(result) {
         console.log(result);
         if (result) {
-          contract.methods.getDataDetails(testingId).call({from: account}).then(function(result){
-            console.log(result[0]);
-            verifyHtml = "True" +"<br>Account: " + result[0] + "<br>Hash: " + result[1] + "<br>Unix Date: " + result[2];
-          });
+          contract.methods
+            .getDataDetails(testingId)
+            .call({ from: account })
+            .then(function(result) {
+              console.log(result[0]);
+              verifyHtml =
+                "True" +
+                "<br>Account: " +
+                result[0] +
+                "<br>Hash: " +
+                result[1] +
+                "<br>Unix Date: " +
+                result[2];
+              $("#getVerifyDiv").html("Verification: " + verifyHtml);
+            })
+            .catch(function(error) {
+              $("#getVerifyDiv").html("Verification: " + verifyHtml);
+            });
         }
-         $("#getVerifyDiv").html("Verification: " + verifyHtml);
+        //$("#getVerifyDiv").html("Verification: " + verifyHtml);
       });
-
-    });
-
-  })
+  });
+});
