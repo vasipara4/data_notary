b56ad39d8a2c3419c8d9072b251bef7f9bd4dc3d
vasipara4 <billprig@gmail.com>
2019-05-08 02:19:58 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,5 @@
 var express = require('express');
 var app = express();
-const multer = require("multer");
-var storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-    cb(null, "/uploads");
-  },
-  filename: function(req, file, cb) {
-    cb(null, file.fieldname + "-" + Date.now());
-  }
-});
-
-var upload = multer({
-  storage: storage,
-  limits: { fileSize: 16 * 1024 * 1024 }
-});
 //var bodyParser = require('body-parser');
 //const fileUpload = require('express-fileupload');
 //var busboy = require('connect-busboy');
