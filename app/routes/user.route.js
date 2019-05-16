3a6cb252bc196726bcc690d3c63833418d94e2dd
vasipara4 <billprig@gmail.com>
2019-05-16 18:37:24 +0300

    Update user.route.js

--- app/routes/user.route.js
+++ app/routes/user.route.js
@@ -71,7 +71,41 @@ module.exports = function(app) {
   app.post("/api/users/save", upload.single("file"), users.save);
 
   //Attach a file to IPFS
-  app.post("/api/ipfs/save", uploadIPFS.single("file"), saveIpfs.save);
+  app.post("/api/ipfs/save", uploadIPFS.single("file"), (req, res) => {
+
+    //MAX_SIZE of file: 16MB
+    const MAX_SIZE = 16777216;
+    const pathOfUpload = req.file.path;
+    const fileSize = req.file.size;
+      if (fileSize > MAX_SIZE) {
+        fs.unlink(pathOfUpload);
+        return res.status(422).json({
+          error: `File needs to be smaller than ${MAX_SIZE} bytes.`,
+        });
+      }
+
+
+    if (!req.file) {
+      return res.status(422).json({
+        error: "File needs to be provided."
+      });
+    }
+
+  console.log("Before IPFS progress");
+    //const data = fs.readFileSync(req.file.path);
+  //  var uploadData = new Buffer(data);
+  var resultFile = ipfs.addFromURL(pathOfUpload, (err, result) => {
+    if (err) {
+      throw err
+    }
+    //fs.unlink(pathOfUpload);
+    console.log(result)
+    res.send(result.hash);
+  });
+
+
+
+  });
 
   // Retrieve all Users' Info
   app.get("/api/users/all", users.findAll);
