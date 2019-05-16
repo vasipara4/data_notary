bcb9f3f6c4d640de403bac9e3b4370553dd56015
vasipara4 <billprig@gmail.com>
2019-05-16 22:25:13 +0300

    Update web3.js

--- resources/static/js/web3.js
+++ resources/static/js/web3.js
@@ -328,7 +328,7 @@ window.addEventListener("load", () => {
       success: function(result) {
           console.log(result);
         $("#resultIPFS").html(
-          "<p>IPFS address: " + result.hash + "</p>"
+          "<p>IPFS address: " + result[0].hash + "</p>"
         );
       },
       error: function(e) {
