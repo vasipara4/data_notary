fb5ae158ccaef7040a29a7aae3dd2b7edef8a1cd
vasipara4 <billprig@gmail.com>
2019-05-16 17:38:52 +0300

    Update user.ipfs.js

--- app/controllers/user.ipfs.js
+++ app/controllers/user.ipfs.js
@@ -2,11 +2,10 @@ exports.save = (req, res) => {
 
   //MAX_SIZE of file: 16MB
   const MAX_SIZE = 16777216;
-
+  const pathOfUpload = req.file.path;
   const fileSize = req.file.size;
     if (fileSize > MAX_SIZE) {
-      fs.unlink(req.file.path);
-
+      fs.unlink(pathOfUpload);
       return res.status(422).json({
         error: `File needs to be smaller than ${MAX_SIZE} bytes.`,
       });
@@ -19,18 +18,16 @@ exports.save = (req, res) => {
     });
   }
 
-  const data = fs.readFileSync(req.file.path);
+  //const data = fs.readFileSync(req.file.path);
+//  var uploadData = new Buffer(data);
+var result = ipfs.addFromURL(pathOfUpload, (err, result) => {
+  if (err) {
+    throw err
+  }
+  fs.unlink(pathOfUpload);
+  console.log(result)
+});
 
-  return ipfs.add(data, (err, files) => {
-    fs.unlink(req.file.path);
-    if (files) {
-      return res.json({
-        hash: files[0].hash
-      });
-    }
+res.send(result);
 
-    return res.status(500).json({
-      error: err
-    });
-  });
 };
