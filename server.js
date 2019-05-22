40ed13bdb5f863cee73634c9135d3223ecd34588
vasipara4 <billprig@gmail.com>
2019-05-22 19:41:43 +0300

    Revert "Update server.js"
    This reverts commit 6d815363ae68a18b3d561565ba1744b4f586b7c9.

--- server.js
+++ server.js
@@ -1,47 +1,41 @@
-var express = require("express");
+var express = require('express');
 var app = express();
-
-//Connecting to ropsten
-global.Web3 = require("web3");
-global.web3 = new Web3(
-  new Web3.providers.HttpProvider("http://localhost:8545")
-);
-web3.eth.net.getNetworkType().then( (result) => console.log("Ethereum network:" + result));
+global.Web3 = require('web3');
+global.web3 = new Web3(new
+Web3.providers.HttpProvider('http://localhost:8545'));
 //const net = require('net');
 
+//const multer = require('multer');
 
-
-app.use(express.static("resources"));
-app.use(express.static("public"));
+app.use(express.static('resources'));
+app.use(express.static('public'));
 
 global.__basedir = __dirname;
 
 // Configuring the database
-const dbConfig = require("./app/config/mongodb.config.js");
-const mongoose = require("mongoose");
+const dbConfig = require('./app/config/mongodb.config.js');
+const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
 // Connecting to the database
-mongoose
-  .connect(
-    dbConfig.url,
-    { useNewUrlParser: true, useCreateIndex: true }
-  )
-  .then(() => {
+mongoose.connect(dbConfig.url, { useNewUrlParser: true, useCreateIndex: true })
+.then(() => {
     console.log("Successfully connected to MongoDB.");
-  })
-  .catch(err => {
-    console.log("Could not connect to MongoDB.");
+}).catch(err => {
+    console.log('Could not connect to MongoDB.');
     process.exit();
-  });
+});
 
-require("./app/routes/user.route.js")(app);
+
+require('./app/routes/user.route.js')(app);
 
 // Create a Server
-var server = app.listen(3008, function() {
-  var host = server.address().address;
-  var port = server.address().port;
+var server = app.listen(3008, function () {
 
-  console.log("App listening at http://%s:%s", host, port);
-});
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("App listening at http://%s:%s", host, port)
+
+})
