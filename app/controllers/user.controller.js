a77c8f0ed13c000e4305903eec91277797c8679a
vasipara4 <billprig@gmail.com>
2019-06-05 22:29:45 +0300

    Update user.controller.js

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -143,7 +143,8 @@ exports.buy = (req, res) => {
   var dateServer = Math.floor(new Date() / 1000);
   console.log("Server Time:" + dateServer);
   console.log("Tx Time:" + req.body.timestamp);
-
+  console.log(url_file);
+  console.log(req.body.submitter);
   //VALIDATION RULES:
   //dateServer can be smaller than block.timestamp for 10 seconds
   //after 150 seconds the POST request is canceled
@@ -163,7 +164,7 @@ exports.buy = (req, res) => {
       message: "Error"
     });
   }
-
+console.log(req.body.gasUsed);
   var id = req.body.id + req.body.submitter;
   // Create a new User model
   const saveToDBbuy = new UserModelDB({
@@ -183,7 +184,8 @@ exports.buy = (req, res) => {
     .call({ from: req.body.submitter })
     .then(function(result) {
       ethereumTimestamp = result[1];
-
+      console.log(ethereumTimestamp);
+      console.log(req.body.timestamp);
       //timestamp must be the same as in Ethereum
       if (req.body.timestamp != ethereumTimestamp) {
         return res.status(400).send({
@@ -195,7 +197,8 @@ exports.buy = (req, res) => {
       saveToDBbuy
         .save()
         .then(data => {
-          res.send(data);
+          console.log("mpike mesa");
+          res.send("confirmed");
         })
         .catch(err => {
           res.status(500).send({
