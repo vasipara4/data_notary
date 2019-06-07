b49811dfb63d4e241f13ee1af0cc6948b71ce07c
vasipara4 <billprig@gmail.com>
2019-06-07 22:43:58 +0300

    Update marketplace.js

--- resources/static/js/marketplace.js
+++ resources/static/js/marketplace.js
@@ -662,14 +662,14 @@ function printMarketplace(
 ) {
   if (i % 3 === 0)
     $("#marketplaceContainer").append(
-      `<div class="row" id="row${i}" style="display:flex;">`
+      `<div class="row" id="row${i}" style="display:flex;flex-wrap: wrap;">`
     );
   $(`#row${parseInt(i/3)}`).append(
     `<div class="col-sm-4">
     <form id="buyItem` +
       i +
-      `" class="formdataBuy" ${_isYours}  style="flex-grow: 1;padding-right:2px;">
-      <div class="card-img"></div><div class="card card-price card-body"><div class="lead">${title}</div><ul class="details"><li>${description}</li><li>
+      `" class="formdataBuy" ${_isYours} > <div class="card card-price" style="flex-grow: 1;padding-right:2px;">
+      <div class="card-img"></div><div class="card-body"><div class="lead">${title}</div><ul class="details"><li>${description}</li><li>
       Extra Content (IPFS):  ${ipfsAddress}  </li><li> Date Inserted: ` +
       date +
       `</li><li> Owner: ${_owner}</li><li>ID of File: ` +
@@ -678,7 +678,7 @@ function printMarketplace(
       `</li></ul><div class="price"><input id="buyItem${i}Value"  type = "hidden" value = "${valueWei}" readonly />` +
       valueWei +
       ` Wei</div><input type="Submit" id="buyItem${i}Submit" class="btn btn-primary btn-md btn-block buy-now" value=${submitButton}>
-      </div> </form></div>`
+      </div></div> </form></div>`
   );
   if (i % 3 === 2) $("#marketplaceContainer").append(`</div>`);
   if (i === length - 1 && i % 3 !== 2)
