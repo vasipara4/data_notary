085ffcf8bbf7b0fc9b2dd1b7a600f2b5ca83111c
vasipara4 <billprig@gmail.com>
2019-06-07 11:15:42 +0300

    Update marketplace.js

--- resources/static/js/marketplace.js
+++ resources/static/js/marketplace.js
@@ -662,7 +662,7 @@ function printMarketplace(
 ) {
   if (i % 3 === 0)
     $("#marketplaceContainer").append(
-      `<div class="row" id="row${i}" style="display:flex; flex-wrap: wrap;justify-content: space-around;">`
+      `<div class="row" id="row${i}" style="display:flex; flex-wrap: wrap;align-content: space-between;">`
     );
   $(`#row${parseInt(i/3)}`).append(
     `<div class="col-sm-4 card card-price" style="flex-grow: 1;">
