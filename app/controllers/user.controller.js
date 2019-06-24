6ffdb10aca962cafd65bbe904c618648eb790690
vasipara4 <billprig@gmail.com>
2019-06-24 23:52:19 +0300

    Revert "Update user.controller.js"
    This reverts commit e206469f32bf8fd01cc3c7c02d3702d71d350999.

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -156,8 +156,8 @@ exports.fileIntegrity = (req, res) => {
       message: "Error"
     });
   }
-address = address.toLowerCase();
-  UserModelDB.findOne({ 'hash': hash, 'submitter': address })
+console.log(hash);
+  UserModelDB.findOne({ hash: hash, submitter: address })
     .then(users => {
       console.log(users);
       var hashOfDb;
